b3aeacb58f6bf7223b9852c81ecd5f2f25b72d58
nel-data07 <nelly.ariib@gmail.com>
2024-11-26 23:01:19 +0100

    mis a jour

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import os
 
 app = Flask(__name__)
 
-# Définir le chemin vers le modèle
+# Charger le modèle LightGBM
 model_path = os.path.join(os.path.dirname(__file__), "lightgbm_model_final.txt")
-model = None  # Modèle initialisé à None, chargé à la demande
+try:
+    model = lgb.Booster(model_file=model_path)
+except Exception as e:
+    raise RuntimeError(f"Erreur lors du chargement du modèle LightGBM : {str(e)}")
 
 @app.route('/')
 def home():
@@ -17,12 +20,7 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     """Endpoint pour effectuer des prédictions."""
-    global model
     try:
-        # Charger le modèle si ce n'est pas déjà fait
-        if model is None:
-            model = lgb.Booster(model_file=model_path)
-
         # Vérifier si les données envoyées sont au format JSON
         if not request.is_json:
             return jsonify({"error": "Les données doivent être au format JSON."}), 400
@@ -33,11 +31,15 @@ def predict():
 
         # Vérifier les colonnes attendues par le modèle
         expected_columns = model.feature_name()
-        for col in expected_columns:
-            if col not in df.columns:
-                df[col] = 0  # Ajouter les colonnes manquantes avec une valeur par défaut
+        missing_cols = [col for col in expected_columns if col not in df.columns]
 
-        # Préserver uniquement les colonnes nécessaires
+        if missing_cols:
+            # Créer un DataFrame avec les colonnes manquantes
+            missing_df = pd.DataFrame(0, index=df.index, columns=missing_cols)
+            # Concaténer les deux DataFrames
+            df = pd.concat([df, missing_df], axis=1)
+
+        # Réorganiser les colonnes pour correspondre à l'ordre attendu
         df = df[expected_columns]
 
         # Faire les prédictions
@@ -51,4 +53,3 @@ if __name__ == '__main__':
     # Utilise le port défini par Render ou par défaut 8000
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
-
