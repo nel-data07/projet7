c47f3202be5f20a7b5d772078e1692b82fa875b6
nel-data07 <nelly.ariib@gmail.com>
2024-11-27 22:49:00 +0100

    correction app.py

--- app.py
+++ app.py
@@ -16,6 +16,25 @@ model = lgb.Booster(model_file=MODEL_PATH)
 # Colonnes attendues par le modèle
 expected_columns = model.feature_name()
 
+# Colonnes manquantes avec valeurs par défaut
+default_columns = {
+    'FLAG_OWN_REALTY': 0, 'REGION_POPULATION_RELATIVE': 0.0, 'DAYS_BIRTH': 0,
+    'DAYS_EMPLOYED': 0, 'DAYS_REGISTRATION': 0, 'DAYS_ID_PUBLISH': 0, 'OWN_CAR_AGE': 0,
+    'FLAG_MOBIL': 1, 'FLAG_EMP_PHONE': 0, 'FLAG_WORK_PHONE': 0, 'FLAG_CONT_MOBILE': 1,
+    'FLAG_PHONE': 0, 'FLAG_EMAIL': 0, 'CNT_FAM_MEMBERS': 1, 'REGION_RATING_CLIENT': 1,
+    'REGION_RATING_CLIENT_W_CITY': 1, 'HOUR_APPR_PROCESS_START': 10, 'REG_REGION_NOT_LIVE_REGION': 0,
+    'REG_REGION_NOT_WORK_REGION': 0, 'LIVE_REGION_NOT_WORK_REGION': 0,
+    'REG_CITY_NOT_LIVE_CITY': 0, 'REG_CITY_NOT_WORK_CITY': 0, 'LIVE_CITY_NOT_WORK_CITY': 0,
+    'EXT_SOURCE_1': 0.0, 'EXT_SOURCE_2': 0.0, 'EXT_SOURCE_3': 0.0,
+    'APARTMENTS_AVG': 0.0, 'BASEMENTAREA_AVG': 0.0, 'YEARS_BEGINEXPLUATATION_AVG': 0.0,
+    'YEARS_BUILD_AVG': 0.0, 'COMMONAREA_AVG': 0.0, 'ELEVATORS_AVG': 0.0,
+    'ENTRANCES_AVG': 0.0, 'FLOORSMAX_AVG': 0.0, 'FLOORSMIN_AVG': 0.0,
+    'LANDAREA_AVG': 0.0, 'LIVINGAPARTMENTS_AVG': 0.0, 'LIVINGAREA_AVG': 0.0,
+    'NONLIVINGAPARTMENTS_AVG': 0.0, 'NONLIVINGAREA_AVG': 0.0,
+    'DAYS_EMPLOYED_PERC': 0.0, 'INCOME_CREDIT_PERC': 0.0, 'INCOME_PER_PERSON': 0.0,
+    'ANNUITY_INCOME_PERC': 0.0, 'PAYMENT_RATE': 0.0
+}
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -26,11 +45,15 @@ def predict():
         # Transformer les données en DataFrame
         logging.info(f"Préparation des données...")
         df = pd.DataFrame(input_data)
- # Ajouter les colonnes manquantes
+
+        # Ajouter les colonnes manquantes
         for col, default_value in default_columns.items():
             if col not in df.columns:
                 df[col] = default_value
         logging.info(f"Données après traitement : {df.head()}")
+
+        # Reindexer pour s'assurer que l'ordre des colonnes correspond au modèle
+        df = df.reindex(columns=expected_columns, fill_value=0)
 
         # Prediction
         predictions = model.predict(df)
